3696cd81fcc28f2a97458b4f8b68a1d684ab413f
Michael Fulton <emu4hire@gmail.com>
2016-02-05 21:43:15 -0500

    Added Google Location Services API, connection testing and resolution

--- app/src/main/java/mfulton/drivedata/Capture.java
+++ app/src/main/java/mfulton/drivedata/Capture.java
@@ -3,10 +3,6 @@ package mfulton.drivedata;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,7 +13,6 @@ import android.os.Looper;
 import android.os.StatFs;
 import android.os.SystemClock;
 import android.util.Log;
-import android.util.StateSet;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -38,20 +33,8 @@ public class Capture {
 
     private boolean capturing;
     private String logName;
-    private File directory, accelFile, locationFile, imageDir;
-    private OutputStream outAccel, outLocation;
-
-    private SensorManager mySensors;
-    private Sensor myAccel;
-    private Sensor orient;
-    private LocationManager local;
-
-    private float accelValues[];
-    private float gravity[];
-    private double locationValues[];
-    private int orientation;
-    private long timestamp;
-    private String provider;
+    private File directory, locationFile, imageDir;
+    private OutputStream outLocation;
 
     private Camera cam;
     private Preview preview;
@@ -62,25 +45,9 @@ public class Capture {
         myContext = context;
         logName = name;
 
-        mySensors = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
-        myAccel = mySensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mySensors.registerListener(mySensorlistener, myAccel, SensorManager.SENSOR_DELAY_FASTEST);
-        orient = mySensors.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        mySensors.registerListener(myOrienter, orient, SensorManager.SENSOR_DELAY_FASTEST);
-
-        local = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-
-        provider = LocationManager.NETWORK_PROVIDER;
-
         directory = filePrep(null, logName, true);
         imageDir = filePrep(directory, "images", true);
-        accelFile = filePrep(directory, (logName + "_ACCEL.log"), false);
         locationFile = filePrep(directory, (logName + "_LOCATION.log"), false);
-
-        accelValues = new float[3];
-        gravity = new float[3];
-        locationValues = new double[5];
-        locationValues[0]=locationValues[1]=locationValues[2]=locationValues[3]=locationValues[4]=0;
 
         try{
             cam = Camera.open();
@@ -94,7 +61,6 @@ public class Capture {
         }
 
         try {
-            outAccel = new FileOutputStream(accelFile);
             outLocation = new FileOutputStream(locationFile);
         }catch (Exception e){
             Log.e("Capture", e.toString());
@@ -118,49 +84,6 @@ public class Capture {
                                 cameraSafe = false;
                                 cam.startPreview();
 
-                                Location location = local.getLastKnownLocation(provider);
-
-                                locationValues[0] = location.getLatitude();
-                                locationValues[1] = location.getLongitude();
-                                locationValues[2] = location.getAltitude();
-                                //locationValues[3] = location.getBearing();
-                                orientation = (int) locationValues[3];
-                                locationValues[4] = location.getAccuracy();
-
-                                //CaptureActivity acceleration
-                                String message;
-                                message = Long.toString(timestamp) + " , "
-                                        + Float.toString(accelValues[0]) + " , "
-                                        + Float.toString(accelValues[1]) + " , "
-                                        + Float.toString(accelValues[2]) + "\n";
-                                outAccel.write(message.getBytes());
-
-                                //CaptureActivity location
-                                message = Long.toString(timestamp) + " , "
-                                        + Double.toString(locationValues[0]) + " , "
-                                        + Double.toString(locationValues[1]) + " , "
-                                        + Double.toString(locationValues[2]) + " , "
-                                        + Integer.toString(orientation) + " , "
-                                        + Double.toString(locationValues[4]) +
-                                        "\n";
-                                outLocation.write(message.getBytes());
-
-
-                                message="( " + Float.toString(accelValues[0]) + ", "
-                                        + Float.toString(accelValues[1]) + ", "
-                                        + Float.toString(accelValues[2]) + " )";
-                                ((TextView) myActivity.findViewById(R.id.accel_indicator)).setText(message);
-
-                                StatFs stats = new StatFs(directory.getPath());
-                                long bytesPossible = (long) stats.getBlockSize() * (long) stats.getBlockCount();
-                                long bytesAvailable = (long) stats.getBlockSize() * (long) stats.getAvailableBlocks();
-                                long megsAvailable = bytesAvailable/ 1048576;
-                                long megsPossible = bytesPossible/ 1048576;
-                                long percentageFree = (megsAvailable/megsPossible) *100;
-
-                                message = Long.toString(percentageFree) + "%  " + " Free";
-
-                                ((TextView) myActivity.findViewById(R.id.space_remaining)).setText(message);
                             }
 
                         } catch (Exception e) {
@@ -181,7 +104,6 @@ public class Capture {
 
         cam.release();
         try {
-            outAccel.close();
             outLocation.close();
         }catch (Exception e){
             Log.e("Capture", e.toString());
@@ -267,48 +189,6 @@ public class Capture {
         //Return the File, tied to a directory of .log file at the correct path..
         return result;
     }
-
-    private SensorEventListener mySensorlistener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
-                accelValues[0] = event.values[0];
-                accelValues[1] = event.values[1];
-                accelValues[2] = event.values[2];
-            }
-
-            else {
-                final float alpha = (float) 0.9;
-                gravity[0] = alpha * gravity[0] + (1-alpha) * event.values[0];
-                gravity[1] = alpha * gravity[1] + (1-alpha) * event.values[1];
-                gravity[2] = alpha * gravity[2] + (1-alpha) * event.values[2];
-
-                accelValues[0] = event.values[0] - gravity[0];
-                accelValues[1] = event.values[1] - gravity[1];
-                accelValues[2] = event.values[2] - gravity[2];
-            }
-
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-    private SensorEventListener myOrienter = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            locationValues[3] = event.values[0];
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
 
     private Camera.PictureCallback myPicture = new Camera.PictureCallback() {
 
--- app/src/main/java/mfulton/drivedata/MainMenu.java
+++ app/src/main/java/mfulton/drivedata/MainMenu.java
@@ -1,56 +1,97 @@
 package mfulton.drivedata;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.support.v4.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainMenu extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+
+public class MainMenu extends FragmentActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+/*
+    OBJECT VARIABLES
+        These values hold information required by the activity.
+*/
+    private GoogleApiClient googleApiClient; //The connection client for the Google API.
+    private boolean criteriaPassed; //The status of testing of the Google Location API.
+
+    private boolean resolvingError;//Is the Activity currently resolving an error?
+
+    //Constants
+    private static final int REQUEST_RESULT_ERROR = 1001; //Request code.
+    private static final String DIALOG_ERROR = "dialog_error"; //Tag for the error dialog fragment.
+    private static final String STATE_RESOLVING_ERROR = "resolving_error"; // Tag for the state resolving state.
+    private static final String STATE_CRITERIA_MET = "criteria_met"; //Tag for the criteria met state.
+
+ /*
+    ACTIVITY LIFECYCLE FUNCTIONS
+        These overriden functions are crucial to maintaining the proper lifecycle of the Activity.
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (googleApiClient == null){
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
         }
-
-        return super.onOptionsItemSelected(item);
+        criteriaPassed = savedInstanceState != null && savedInstanceState.getBoolean(STATE_CRITERIA_MET, false);
+        resolvingError = savedInstanceState != null && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
     }
+
+    @Override
+    protected void onStart(){
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_RESOLVING_ERROR, resolvingError);
+        outState.putBoolean(STATE_CRITERIA_MET, criteriaPassed);
+    }
+
+    @Override
+    protected void onStop(){
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
     @Override
     public void onDestroy(){
-        Log.i("MainMenu", "is terminating nicely.");
-        return;
+        super.onDestroy();
     }
 
+/*
+    HANDLERS FOR UI INTERACTIONS
+        The following functions handle button presses and other interaction.
+ */
 
     public void startCapture(View view){
         View parent = view.getRootView();
-
         TextView logEntry = (TextView) parent.findViewById(R.id.logNameEntry);
-
-
         CharSequence logName = logEntry.getText();
         String logString = logName.toString();
 
@@ -61,11 +102,107 @@ public class MainMenu extends Activity {
 
         Log.i("MainMenu", "Log Name is " + logString);
 
-
         Intent intent= new Intent(this, CaptureActivity.class);
         intent.putExtra("logName", logString);
         startActivity(intent);
     }
 
+/*
+    CONNECTION HANDLERS FOR GOOGLE API
+ */
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+        criteriaPassed = checkCriteria();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause){
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+        if(resolvingError){
+            return; //An error is being resolved.  We need to wait.
+        }
+        else if(result.hasResolution()){ //There is a resolution available.
+            try{
+                    resolvingError = true;
+                    result.startResolutionForResult(this, REQUEST_RESULT_ERROR);
+            }catch(IntentSender.SendIntentException e){
+                //There was an error attempting to resolve.  Let's try again.
+                googleApiClient.connect();
+            }
+        }
+        else{
+            //Show error dialog
+            resolvingError = true;
+            showErrorDialog(result.getErrorCode());
+
+        }
+    }
+
+    //The following functions and inline classes are defined to help resolve connection issues.
+    public void showErrorDialog(int errorCode){
+        //Create a new fragment.
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        //Pass in the error that must be displayed.
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    // Called once the ErrorDialogFragment is done.
+    public void onDialogDismissed(){
+        resolvingError = false;
+    }
+
+    //Class used to select the correct dialog
+    public static class ErrorDialogFragment extends DialogFragment{
+        public ErrorDialogFragment(){}
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            //Get the error code
+            int errorCode =  this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESULT_ERROR
+            );
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog){
+            ((MainMenu) getActivity()).onDialogDismissed();
+        }
+    }
+
+    //Once the error resolution completes, this callback is needed
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if( requestCode == REQUEST_RESULT_ERROR){
+            resolvingError = false;
+            if(resultCode == RESULT_OK){
+                if(!googleApiClient.isConnecting() &&
+                        !googleApiClient.isConnected()){
+                    googleApiClient.connect();
+                }
+            }
+        }
+    }
+
+/*
+    END OF ERROR PROCESSING
+*/
+    //Checking the criteria
+    protected boolean checkCriteria(){
+        LocationRequest request = new LocationRequest();
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request);
+        PendingResult<LocationSettingsRequest> result = LocationServices.SettingsApi.
+                checkLocationSettings(googleApiClient, builder.build());
+        return false;
+    }
 
 }
