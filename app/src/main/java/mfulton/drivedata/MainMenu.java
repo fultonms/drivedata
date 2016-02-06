29d3fb767a78c220e727ee0c70815f7e2254c0ba
Michael Fulton <emu4hire@gmail.com>
2016-02-05 23:34:40 -0500

    finished addition of ApiClient to MainMenu

--- app/src/main/java/mfulton/drivedata/MainMenu.java
+++ app/src/main/java/mfulton/drivedata/MainMenu.java
@@ -1,8 +1,7 @@
 package mfulton.drivedata;
 
-import android.app.Activity;
 import android.app.Dialog;
-import android.content.BroadcastReceiver;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,46 +9,50 @@ import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.ErrorDialogFragment;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class MainMenu extends FragmentActivity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-/*
-    OBJECT VARIABLES
-        These values hold information required by the activity.
-*/
+    /*
+        OBJECT VARIABLES
+            These values hold information required by the activity.
+    */
     private GoogleApiClient googleApiClient; //The connection client for the Google API.
     private boolean criteriaPassed; //The status of testing of the Google Location API.
 
     private boolean resolvingError;//Is the Activity currently resolving an error?
 
     //Constants
-    private static final int REQUEST_RESULT_ERROR = 1001; //Request code.
+    private static final int REQUEST_RESULT_ERROR = 1001; //Request code for errors.
+    private static final int REQUEST_CHECK_SETTINGS = 0x1; //Check settings requet.
     private static final String DIALOG_ERROR = "dialog_error"; //Tag for the error dialog fragment.
     private static final String STATE_RESOLVING_ERROR = "resolving_error"; // Tag for the state resolving state.
     private static final String STATE_CRITERIA_MET = "criteria_met"; //Tag for the criteria met state.
 
- /*
-    ACTIVITY LIFECYCLE FUNCTIONS
-        These overriden functions are crucial to maintaining the proper lifecycle of the Activity.
- */
+    /*
+       ACTIVITY LIFECYCLE FUNCTIONS
+           These overriden functions are crucial to maintaining the proper lifecycle of the Activity.
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        if (googleApiClient == null){
+        if (googleApiClient == null) {
             googleApiClient = new GoogleApiClient.Builder(this)
                     .addApi(LocationServices.API)
                     .addConnectionCallbacks(this)
@@ -61,26 +64,26 @@ public class MainMenu extends FragmentActivity
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         googleApiClient.connect();
         super.onStart();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(STATE_RESOLVING_ERROR, resolvingError);
         outState.putBoolean(STATE_CRITERIA_MET, criteriaPassed);
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         googleApiClient.disconnect();
         super.onStop();
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
     }
 
@@ -89,20 +92,20 @@ public class MainMenu extends FragmentActivity
         The following functions handle button presses and other interaction.
  */
 
-    public void startCapture(View view){
+    public void startCapture(View view) {
         View parent = view.getRootView();
         TextView logEntry = (TextView) parent.findViewById(R.id.logNameEntry);
         CharSequence logName = logEntry.getText();
         String logString = logName.toString();
 
-        if(logString.isEmpty()){
+        if (logString.isEmpty()) {
             logEntry.setError("This field cannot be empty");
             return;
         }
 
         Log.i("MainMenu", "Log Name is " + logString);
 
-        Intent intent= new Intent(this, CaptureActivity.class);
+        Intent intent = new Intent(this, CaptureActivity.class);
         intent.putExtra("logName", logString);
         startActivity(intent);
     }
@@ -112,29 +115,62 @@ public class MainMenu extends FragmentActivity
  */
 
     @Override
-    public void onConnected(Bundle connectionHint){
-        criteriaPassed = checkCriteria();
+    public void onConnected(Bundle connectionHint) {
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(5000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request);
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.
+                checkLocationSettings(googleApiClient, builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates states = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        //All location settings are good!
+                        criteriaPassed = true;
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        //There are some issues.
+                        try {
+                            status.startResolutionForResult(
+                                    MainMenu.this,
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.e("DriveData", e.toString());
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        //Location settings are wrong, but cannot be resolved.
+                        criteriaPassed = false;
+                        break;
+                }
+            }
+        });
     }
 
     @Override
-    public void onConnectionSuspended(int cause){
+    public void onConnectionSuspended(int cause) {
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result){
-        if(resolvingError){
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        if (resolvingError) {
             return; //An error is being resolved.  We need to wait.
-        }
-        else if(result.hasResolution()){ //There is a resolution available.
-            try{
-                    resolvingError = true;
-                    result.startResolutionForResult(this, REQUEST_RESULT_ERROR);
-            }catch(IntentSender.SendIntentException e){
+        } else if (result.hasResolution()) { //There is a resolution available.
+            try {
+                resolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESULT_ERROR);
+            } catch (IntentSender.SendIntentException e) {
                 //There was an error attempting to resolve.  Let's try again.
                 googleApiClient.connect();
             }
-        }
-        else{
+        } else {
             //Show error dialog
             resolvingError = true;
             showErrorDialog(result.getErrorCode());
@@ -143,7 +179,7 @@ public class MainMenu extends FragmentActivity
     }
 
     //The following functions and inline classes are defined to help resolve connection issues.
-    public void showErrorDialog(int errorCode){
+    public void showErrorDialog(int errorCode) {
         //Create a new fragment.
         ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
         //Pass in the error that must be displayed.
@@ -154,55 +190,41 @@ public class MainMenu extends FragmentActivity
     }
 
     // Called once the ErrorDialogFragment is done.
-    public void onDialogDismissed(){
+    public void onDialogDismissed() {
         resolvingError = false;
     }
 
     //Class used to select the correct dialog
-    public static class ErrorDialogFragment extends DialogFragment{
-        public ErrorDialogFragment(){}
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() {
+        }
 
         @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState){
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
             //Get the error code
-            int errorCode =  this.getArguments().getInt(DIALOG_ERROR);
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
             return GoogleApiAvailability.getInstance().getErrorDialog(
                     this.getActivity(), errorCode, REQUEST_RESULT_ERROR
             );
         }
 
         @Override
-        public void onDismiss(DialogInterface dialog){
+        public void onDismiss(DialogInterface dialog) {
             ((MainMenu) getActivity()).onDialogDismissed();
         }
     }
 
     //Once the error resolution completes, this callback is needed
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if( requestCode == REQUEST_RESULT_ERROR){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESULT_ERROR) {
             resolvingError = false;
-            if(resultCode == RESULT_OK){
-                if(!googleApiClient.isConnecting() &&
-                        !googleApiClient.isConnected()){
+            if (resultCode == RESULT_OK) {
+                if (!googleApiClient.isConnecting() &&
+                        !googleApiClient.isConnected()) {
                     googleApiClient.connect();
                 }
             }
         }
     }
-
-/*
-    END OF ERROR PROCESSING
-*/
-    //Checking the criteria
-    protected boolean checkCriteria(){
-        LocationRequest request = new LocationRequest();
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(request);
-        PendingResult<LocationSettingsRequest> result = LocationServices.SettingsApi.
-                checkLocationSettings(googleApiClient, builder.build());
-        return false;
-    }
-
 }
